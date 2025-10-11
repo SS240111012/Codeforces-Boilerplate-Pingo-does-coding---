6842083f6413d9ab17dd7b2af49958f7094d5e97
Mr. GhaniX <ghanisarfraz999@gmail.com>
2025-10-11 18:04:28 +0500

    Codeforces Boilerplate
    Content :
    - No shortcut STL like <bits.etc> thing, instead all the common and needed Codeforces STLS so loading time stays lower .
    - FastIo and endl as "\n" 
    - void solve() pre made 
    - Use and code plate
    - Easy and intuitive for new comers and alright for peers / Masters (although they will use their own one lol)
    - Change it or modify it as how ever as you want

---
+++ code.cpp
@@ -0,0 +1,72 @@
+/*
+
+Template for codeforces : vi ./etc/default/rating/.config // vi == vim
+
+$sudo : ➜  ~  800 ~~~ 1000
+
+$sudo @user : ➜  ~  Pingo_does_coding
+
+$sudo man contest_divion.exe :
+  password : ➜  ~  *********
+  
+  ➜  ~  Division : _____ || problem : _____ || Name : _____
+
+
+*/
+
+// ➜  ~  ---------------------------- 被你发现了
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <limits>    
+#include <iomanip>   
+
+using namespace std;
+
+// ➜  ~ Fast IO
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+#define endl "\n"
+
+// Type aliases
+using ll = long long;
+
+
+// ➜  ~  ----------------------------
+
+void solve()
+{
+ //functions here if you want 
+
+}
+
+int main() {
+    fast_io;
+
+    // code here 
+
+return 0 ;
+}
+
+
+// ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷   
+// ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇    
+// ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽    
+// ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕    
+// ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕    
+// ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕    
+// ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄    
+// ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕    
+// ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿
+// ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿       
+// ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟
+// ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠
+// ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙
+// ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ 
