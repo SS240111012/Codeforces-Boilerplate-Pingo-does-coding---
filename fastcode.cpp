a549233ac3e568fa39df0b4650023238f6725b58
Mr. GhaniX <ghanisarfraz999@gmail.com>
2025-11-07 20:49:27 +0500

    Clarify namespace usage in fastcode.cpp
    Made a new Version of the pre - exsisting template called 
    
    "Code.cpp"
    
    more description in readme. stuff

---
+++ fastcode.cpp
@@ -0,0 +1,62 @@
+/* 
+  -> • Codeforces , Atcoder, Leetcode, Codechef, Codewars .
+  -> • Template: Pingo_does_coding
+  -> • Division: 2  || Problem: A || Name: I am not writing names lmao .
+
+*/
+
+/*
+  -> sudo ls Programming .
+  -> :: [sudo] Password for Arch : Im a mother**king starboy (no harass intended)
+  -> :: Founded Files, folders, programms :->
+     :: code.cpp  code.exe  mycode.py
+     
+  -> sudo nvim code.cpp
+  
+*/
+
+
+
+
+/*  •  My libraries and STLs  •  */
+namespace STLs {
+    #include <iostream>
+    #include <vector>
+    #include <algorithm>
+    #include <cmath>
+    #include <string>
+    #include <set>
+    #include <map>
+    #include <queue>
+    #include <stack>
+    #include <limits>
+    #include <iomanip>
+
+    using namespace std;
+    using ll = long long;
+}
+// make sure to close the namespace STLs function to be less cluttered ;
+
+/*  •  For blazing Fast Input Output (IO) speeds  •  */
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl "\n"
+
+using namespace std;
+using ll = long long;
+using namespace STLs ;
+
+/*  • My Fucntions  •  */
+
+void solve() 
+{
+  // code here 
+  //int t , cin >> t ;
+  int t ;
+}
+int main() // or int32_t ; 
+{
+  fast_io ;
+  solve() ;
+}
+
+
